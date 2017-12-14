db5ed7d59dcc32929b5cfb550796376b2322b2fb
sterddev <sterddev@icloud.com>
2017-12-15 00:38:59 +0900

    Design MainActivity

--- app/src/main/java/net/sterd/osungapp/MainActivity.java
+++ app/src/main/java/net/sterd/osungapp/MainActivity.java
@@ -1,13 +1,24 @@
 package net.sterd.osungapp;
 
+import android.graphics.Color;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.colorAccent));
+        toolbar.setTitle("OSUNGAPP");
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
     }
 }
